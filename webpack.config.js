de7b0986e8f01d106224700d486ecdc59d730f26
Whitefur Wang <whitefur.wang@gmail.com>
2018-07-01 00:33:18 +0800

    Add clean-webpack-plugin to clean old css file in demo dir.

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,29 @@ const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 const devMode = process.env.NODE_ENV !== 'production'
+
+let plugins = [
+  new HtmlWebPackPlugin({
+    template: './src/index.html',
+    filename: './index.html'
+  }),
+  new MiniCssExtractPlugin({
+    // Options similar to the same options in webpackOptions.output
+    // both options are optional
+    filename: '[name].[hash].css',
+    chunkFilename: '[id].[hash].css'
+  }),
+  new CopyWebpackPlugin([
+    { from: 'src/assets/images', to: 'assets/images' }
+  ])
+]
+
+if (!devMode) {
+  plugins = plugins.concat([new CleanWebpackPlugin('demo/*.css')])
+}
 
 module.exports = {
   entry: './src/index.js',
@@ -50,19 +71,5 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html'
-    }),
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: '[name].[hash].css',
-      chunkFilename: '[id].[hash].css'
-    }),
-    new CopyWebpackPlugin([
-      { from: 'src/assets/images', to: 'assets/images' }
-    ])
-  ]
+  plugins: plugins
 }
